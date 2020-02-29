9f6987a2ae73fff032c67f73ebc3dce75387c8e5
unknown <hamediran1999@gmail.com>
2020-02-29 17:04:48 +0330

    add post form

--- core/resume/forms.py
+++ core/resume/forms.py
@@ -1,3 +1,10 @@
 from django import forms
+from .models import Article
 
-  
+class SendForm(forms.Form):
+    tittle = forms.CharField(max_length=128)
+    slug = forms.SlugField()
+    message = forms.CharField(widget=forms.Textarea)
+    category = forms.ModelChoiceField(queryset=Article.category_list.objects)
+    aurhor = forms.CharField(max_length=128)
+    email = forms.EmailField()
